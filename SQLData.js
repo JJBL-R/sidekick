0ec3da112dea76498f9f4b1cd0fb6eb1776eb0ab
louislam <louislam7229@gmail.com>
2022-10-31 13:53:34 -0700

    link SQL database

--- SQLData.js
+++ SQLData.js
@@ -4,3 +4,10 @@ const PG_URI =
   'postgres://dzqxnoir:lDahHIw4romfWrfZIbIoJGoBhk4RxlUi@heffalump.db.elephantsql.com/dzqxnoir';
 
 const pool = new Pool({ connectionString: PG_URI });
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
