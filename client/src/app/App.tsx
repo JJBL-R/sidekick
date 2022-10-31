7a196323edbd619d34978790378a585559b0102e
Brian <yangbrian310@gmail.com>
2022-10-31 17:14:23 -0400

    create a register page

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,8 +1,15 @@
 import Landing from "../pages/Landing"
+import Register from "../pages/Register"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Landing />
+    <Router>
+      <Routes>
+    <Route path = "/login" element={<Landing />}/>
+    <Route path = "/register" element={<Register />}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserSignupForm from '../components/UserSignupForm/UserSignupForm'
+
+const Register = () => {
+  return (
+    <div>
+      <UserSignupForm/>
+    </div>
+  )
+}
+
+export default Register
+
