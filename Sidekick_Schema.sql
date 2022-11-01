2be3dbed938c870fcf14d8c3017fb04128c97084
louislam <louislam7229@gmail.com>
2022-11-01 14:08:05 -0700

    update user table

--- Sidekick_Schema.sql
+++ Sidekick_Schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE public.user
 	"age" INT NOT NULL,
 	"email" VARCHAR(255) NOT NULL,
 	"zipcode" INT NOT NULL,
+  "facebook_id" VARCHAR NOT NULL,
+  "registered" BOOLEAN,
 	PRIMARY KEY ("_id")
 ) WITH (
   OIDS=FALSE
@@ -18,7 +20,7 @@ CREATE TABLE public.user_sport
 	"user_id" INT NOT NULL,
 	"sport_id" INT NOT NULL,
   PRIMARY KEY ("_id")
-  FOREIGN KEY ("user_id") REFERENCES public.user("_id"),
+  FOREIGN KEY ("user_id") REFERENCES public.user("_id")
   FOREIGN KEY ("sport_id") REFERENCES public.sport("_id")
 ) WITH (
   OIDS=FALSE
@@ -40,7 +42,7 @@ CREATE TABLE public.like_user
   "user_id_given" INT NOT NULL,
   "user_id_received" INT NOT NULL,
   "like" BOOLEAN,
-  FOREIGN KEY ("user_id_given") REFERENCES public.user("_id"),
+  FOREIGN KEY ("user_id_given") REFERENCES public.user("_id")
   FOREIGN KEY ("user_id_received") REFERENCES public.user("_id")
 ) WITH (
   OIDS=FALSE
@@ -52,15 +54,15 @@ CREATE TABLE public.dislike_user
   "user_id_given" INT NOT NULL,
   "user_id_received" INT NOT NULL,
   "dislike" BOOLEAN,
-  FOREIGN KEY ("user_id_given") REFERENCES public.user("_id"),
+  FOREIGN KEY ("user_id_given") REFERENCES public.user("_id")
   FOREIGN KEY ("user_id_received") REFERENCES public.user("_id")
 ) WITH (
   OIDS=FALSE
 );
 
-INSERT INTO public.user(first_name, last_name, bio, age, email, zipcode)
-VALUES ('Michael', 'Scott', 'I am compeittive.', '25', 'joe@gmail.com', '90026'),
-       ('Jim', 'Halpert', 'I like to hike.', '28', 'angel@gmail.com', '90026');
+INSERT INTO public.user(first_name, last_name, bio, age, email, zipcode, facebook_id, registered)
+VALUES ('Michael', 'Scott', 'I am compeittive.', '25', 'joe@gmail.com', '90026', 'f92hg923gh8', true),
+       ('Jim', 'Halpert', 'I like to hike.', '28', 'angel@gmail.com', '90026', '2389gh248', true);
 
 INSERT INTO public.sport(sport_name, registered)
 VALUES ('Baksetball', false),
