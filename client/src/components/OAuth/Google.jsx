be47e6869ad886fbc4a9d703d96fd5679cc39d3e
JP Moregain <j.p.moregain15@gmail.com>
2022-11-02 12:19:52 -0400

    Add user context for global user state management. Invoke setUser to update state on successful OAuth login

--- client/src/components/OAuth/Google.jsx
+++ client/src/components/OAuth/Google.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import UserContext from '../../app/UserContext.tsx';
+import { useContext } from 'react';
 
 const clientId = "600150108739-1hcnbkbmbctp79as2rjbk689efsh1mjb.apps.googleusercontent.com"
 
 const Google = () => {
+  const { user, setUser } = useContext(UserContext)
   const onSuccess = (res) => {
     console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
+    setUser(res.profileObj);
   }
 
   const onFailure = (res) => {
