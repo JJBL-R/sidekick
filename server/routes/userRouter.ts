9ad00394980411d7845d7b8876ac923b7c7f3216
louislam <louislam7229@gmail.com>
2022-11-01 20:29:47 -0700

    create sportController

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -8,9 +8,10 @@ router.post('/', userController.verifyUser, userController.createUser, (req, res
 
 router.patch('/', userController.updateUser, (req, res) => {
   res.status(200).json(res.locals.update);
-})
+});
 
 router.delete('/', userController.deleteUser, (req, res) => {
   res.status(200).json(res.locals.delete);
-})
+});
+
 export default router;
