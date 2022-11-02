89c4a89c914df6d6711e84d4a658e80647db3f33
Jigar Patel <jigarxp@gmail.com>
2022-11-02 11:52:40 -0400

    fix google oauth button styling

--- client/src/components/OAuth/Google.jsx
+++ client/src/components/OAuth/Google.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
+import './OAuth.scss';
 
-const clientId = "600150108739-1hcnbkbmbctp79as2rjbk689efsh1mjb.apps.googleusercontent.com"
+const clientId =
+  '600150108739-1hcnbkbmbctp79as2rjbk689efsh1mjb.apps.googleusercontent.com';
 
 const Google = () => {
   const onSuccess = (res) => {
-    console.log("LOGIN SUCCESS! Current user: ", res.profileObj);
-  }
+    console.log('LOGIN SUCCESS! Current user: ', res.profileObj);
+  };
 
   const onFailure = (res) => {
-    console.log("LOGIN FAILED! res: ", res)
-  }
-  
+    console.log('LOGIN FAILED! res: ', res);
+  };
+
   return (
-    <div className="loginBtn loginBtn--google">
-      <GoogleLogin
-        clientId={clientId}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-      />
-    </div>
+    <GoogleLogin
+      clientId={clientId}
+      buttonText="Login"
+      render={(renderProps) => (
+        <button
+          className="loginBtn loginBtn--google"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          Login with Google
+        </button>
+      )}
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy={'single_host_origin'}
+      isSignedIn={true}
+    />
   );
 };
 
